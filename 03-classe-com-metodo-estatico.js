7c0fc6e36505e17b1d6dc86f7757c4d7fc4238ec
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-30 16:00:14 +0000

    Exemplo

--- 03-classe-com-metodo-estatico.js
+++ 03-classe-com-metodo-estatico.js
@@ -1,3 +1,5 @@
+import promptSync from 'prompt-sync';
+
 class Retangulo {
     static area(base, altura) {
         let resp = parseFloat(base) * parseFloat(altura); 
@@ -5,4 +7,10 @@ class Retangulo {
     }
 }
 
-console.log("Área: " + Retangulo.area(6, 7));
+// Inicia a função de prompt:
+const prompt = promptSync();
+
+let base = prompt("Informe a base do retângulo: ");
+let altura = prompt("Informe a altura do retângulo: ");
+
+console.log("Área: " + Retangulo.area(base, altura));
